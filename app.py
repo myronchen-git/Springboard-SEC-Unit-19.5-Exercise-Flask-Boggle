73d894354685b3ab08325c13419ef05bfe75e69c
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-21 13:47:14 -0800

    feat: set up infrastructure and tools

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
 from boggle import Boggle
 
+# ==================================================
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+
+debug = DebugToolbarExtension(app)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+
 boggle_game = Boggle()
+
+# --------------------------------------------------
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from unittest import TestCase
-from app import app
+
 from flask import session
+
+from app import app
 from boggle import Boggle
+
+app.config["TESTING"] = True
+app.config["DEBUG_TB_HOSTS"] = ["dont-show-debug-toolbar"]
 
 
 class FlaskTests(TestCase):
+    """Tests for the game."""
 
     # TODO -- write tests for every view function / feature!
-
