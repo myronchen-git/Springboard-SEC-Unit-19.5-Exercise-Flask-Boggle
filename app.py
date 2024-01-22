62b577a8fdaf1c72d9b5996c09769412abce2004
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-21 16:47:56 -0800

    feat: create and display board
    (Completed step 2.)
    
    * Added a route to create and display the game board, and save the
    board to the Flask session.
    
    * Added a route at the root path that will redirect to the game route.
    This root path will serve as a configuration screen in the future.
    
    * Created the game.html that will display the game board and the form
    for a user to guess a word.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from boggle import Boggle
@@ -14,3 +14,21 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 boggle_game = Boggle()
 
 # --------------------------------------------------
+
+
+@app.route("/")
+def route_root():
+    """Currently redirects to the game.  In the future, this will display a page to input board size."""
+
+    return redirect("/game")
+
+
+@app.route("/game")
+def route_game():
+    """Displays the game board and a form to submit a word."""
+
+    board = boggle_game.make_board()
+
+    session["board"] = board
+
+    return render_template("game.html", board=board)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import string
 from unittest import TestCase
 
 from flask import session
@@ -12,4 +13,40 @@ app.config["DEBUG_TB_HOSTS"] = ["dont-show-debug-toolbar"]
 class FlaskTests(TestCase):
     """Tests for the game."""
 
-    # TODO -- write tests for every view function / feature!
+    def test_root_redirect(self):
+        with app.test_client() as client:
+            resp = client.get("/")
+
+            self.assertEqual(resp.status_code, 302)
+            self.assertEqual(resp.location, "http://localhost/game")
+
+    def test_board_creation(self):
+        with app.test_client() as client:
+            resp = client.get("/game")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertIn("<table>", html)
+            self.assertIn("<tr>", html)
+            self.assertIn("<td>", html)
+
+    def test_board_saving_in_session(self):
+        with app.test_client() as client:
+            resp = client.get("/game")
+
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertEqual(len(session["board"]), 5)
+            self.assertEqual(len(session["board"][0]), 5)
+            self.assertIn(session["board"][0][0], string.ascii_uppercase)
+
+    def test_form_creation(self):
+        with app.test_client() as client:
+            resp = client.get("/game")
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+
+            self.assertIn("<form>", html)
+            self.assertIn('name="word"', html)
